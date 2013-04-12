b67356d57234438d640eb8677098250de4f99ff0
y.furukawa <y.furukawa@lexues.co.jp>
2013-04-12 20:52:11 +0900

    FIXED for "Declaration of KtaiComponent::initialize() should be compatible with Component::initialize(Controller $controller)" and others.

--- Controller/Component/KtaiComponent.php
+++ Controller/Component/KtaiComponent.php
@@ -1,14 +1,14 @@
 <?php
 /*
  * CakeKtaiLibrary, supports Japanese mobile phone sites coding.
- * This provides many functions such as a carrier check to use Referer or E-mail, 
+ * This provides many functions such as a carrier check to use Referer or E-mail,
  * conversion of an Emoji, and more.
  *
  * PHP versions 5
  *
  * CakeKtaiLibrary for CakePHP2.x
  * Copyright 2009-2012, ECWorks.
- 
+
  * Licensed under The GNU General Public Licence
  * Redistributions of files must retain the above copyright notice.
  *
@@ -33,7 +33,7 @@ if(!class_exists('lib3gk')){
  * @subpackage    KtaiLibrary.app.controllers.components
  */
 class KtaiComponent extends Component {
-	
+
 	//================================================================
 	//Properties
 	//================================================================
@@ -44,7 +44,7 @@ class KtaiComponent extends Component {
 	 * @access protected
 	 */
 	var $_lib3gk = null;
-	
+
 	/**
 	 * コントローラのインスタンス
 	 *
@@ -52,7 +52,7 @@ class KtaiComponent extends Component {
 	 * @access protected
 	 */
 	var $_controller = null;
-	
+
 	/**
 	 * Ktai Libraryパラメータ
 	 *
@@ -60,21 +60,21 @@ class KtaiComponent extends Component {
 	 * @access protected
 	 */
 	var $_options = array(
-		'enable_ktai_session' => true, 
-		'use_redirect_session_id' => false, 
-		'imode_session_name' => 'csid', 
-		'session_save' => 'php', 
-		
-		'output_auto_encoding' => false, 
-		'output_auto_convert_emoji' => false, 
-		'output_convert_kana' => false, 
-		
-		'img_emoji_url' => "/img/emoticons/", 
-		
-		'use_xml' => false, 
+		'enable_ktai_session' => true,
+		'use_redirect_session_id' => false,
+		'imode_session_name' => 'csid',
+		'session_save' => 'php',
+
+		'output_auto_encoding' => false,
+		'output_auto_convert_emoji' => false,
+		'output_convert_kana' => false,
+
+		'img_emoji_url' => "/img/emoticons/",
+
+		'use_xml' => false,
 	);
-	
-	
+
+
 	//================================================================
 	//Methods
 	//================================================================
@@ -88,13 +88,13 @@ class KtaiComponent extends Component {
 	 * @return (なし)
 	 * @access public
 	 */
-	function initialize(&$controller){
-		
-		$this->_controller = &$controller;
-		
-		$this->_options['input_encoding'] = 
+	function initialize(Controller $controller){
+
+		$this->_controller = $controller;
+
+		$this->_options['input_encoding'] =
 		$this->_options['output_encoding'] = Configure::read('App.encoding');
-		
+
 		$this->_lib3gk = Lib3gk::get_instance();
 		$this->_lib3gk->_url_callback = array($this, 'url_callback_func');
 		$this->_lib3gk->_params = array_merge($this->_lib3gk->_params, $this->_options);
@@ -126,8 +126,8 @@ class KtaiComponent extends Component {
 		}
 
 	}
-	
-	
+
+
 	/**
 	 * beforeRenderコールバック
 	 *
@@ -135,7 +135,7 @@ class KtaiComponent extends Component {
 	 * @return (なし)
 	 * @access public
 	 */
-	function beforeRender(&$controller){
+	function beforeRender(Controller $controller){
 		if(isset($controller->ktai)){
 			Configure::write('Ktai', $this->_options);
 		}
@@ -143,8 +143,8 @@ class KtaiComponent extends Component {
 			header('Content-type: application/xhtml+xml');
 		}
 	}
-	
-	
+
+
 	/**
 	 * shutdownコールバック
 	 *
@@ -152,45 +152,45 @@ class KtaiComponent extends Component {
 	 * @return (なし)
 	 * @access public
 	 */
-	function shutdown(&$controller){
-		
+	function shutdown(Controller $controller){
+
 		//自動変換処理
 		//requestAction()からのコールの場合は以下処理をスキップする
 		//
 		if (!isset($controller->params['requested']) || $controller->params['requested'] !== 1) {
 			$out = $controller->response->body();
-			
+
 			$input_encoding  = $this->_options['input_encoding'];
 			$output_encoding = $this->_options['output_encoding'];
-			
+
 			if($this->_options['output_convert_kana'] != false){
 				$out = mb_convert_kana(
-					$out, 
-					$this->_options['output_convert_kana'], 
+					$out,
+					$this->_options['output_convert_kana'],
 					$input_encoding
 				);
 			}
-			
+
 			if($this->_options['output_auto_convert_emoji']){
 				$this->convert_emoji($out);
 			}else{
-				if($this->_options['output_auto_encoding'] && 
+				if($this->_options['output_auto_encoding'] &&
 					($input_encoding != $output_encoding)){
 					$out = mb_convert_encoding(
-						$out, 
-						$output_encoding, 
+						$out,
+						$output_encoding,
 						$input_encoding
 					);
 				}
 			}
-			
+
 			$controller->response->body($out);
 		}
-	
+
 		$this->_lib3gk->shutdown();
 	}
-	
-	
+
+
 	//------------------------------------------------
 	//Ktai Library methods
 	//------------------------------------------------
@@ -204,8 +204,8 @@ class KtaiComponent extends Component {
 	function url_callback_func($url){
 		return Router::url($url);
 	}
-	
-	
+
+
 	/**
 	 * バージョンの入手
 	 * 詳しくはLib3gk::get_version()を参照
@@ -216,8 +216,8 @@ class KtaiComponent extends Component {
 	function get_version(){
 		return $this->_lib3gk->get_version();
 	}
-	
-	
+
+
 	/**
 	 * ユーザエージェントの解析
 	 * 詳しくはLib3gkCarrier::analyze_user_agent()を参照
@@ -229,8 +229,8 @@ class KtaiComponent extends Component {
 	function analyze_user_agent($user_agent = null){
 		return $this->_lib3gk->analyze_user_agent($user_agent);
 	}
-	
-	
+
+
 	/**
 	 * キャリア番号の入手
 	 * 詳しくはLib3gkCarrier::get_carrier()を参照
@@ -243,8 +243,8 @@ class KtaiComponent extends Component {
 	function get_carrier($user_agent = null, $refresh = false){
 		return $this->_lib3gk->get_carrier($user_agent, $refresh);
 	}
-	
-	
+
+
 	/**
 	 * docomo端末かのチェック
 	 * 詳しくはLib3gkCarrier::is_imode()を参照
@@ -255,8 +255,8 @@ class KtaiComponent extends Component {
 	function is_imode(){
 		return $this->_lib3gk->is_imode();
 	}
-	
-	
+
+
 	/**
 	 * SoftBank端末かのチェック
 	 * 詳しくはLib3gkCarrier::is_softbank()を参照
@@ -267,8 +267,8 @@ class KtaiComponent extends Component {
 	function is_softbank(){
 		return $this->_lib3gk->is_softbank();
 	}
-	
-	
+
+
 	/**
 	 * vodafone端末かのチェック
 	 * 詳しくはLib3gkCarrier::is_vodafone()を参照
@@ -279,8 +279,8 @@ class KtaiComponent extends Component {
 	function is_vodafone(){
 		return $this->_lib3gk->is_vodafone();
 	}
-	
-	
+
+
 	/**
 	 * J-PHONE端末かのチェック
 	 * 詳しくはLib3gkCarrier::is_jphone()を参照
@@ -291,8 +291,8 @@ class KtaiComponent extends Component {
 	function is_jphone(){
 		return $this->_lib3gk->is_jphone();
 	}
-	
-	
+
+
 	/**
 	 * AU端末かのチェック
 	 * 詳しくはLib3gkCarrier::is_ezweb()を参照
@@ -303,8 +303,8 @@ class KtaiComponent extends Component {
 	function is_ezweb(){
 		return $this->_lib3gk->is_ezweb();
 	}
-	
-	
+
+
 	/**
 	 * EMOBILE端末かのチェック
 	 * 詳しくはLib3gkCarrier::is_emobile()を参照
@@ -315,8 +315,8 @@ class KtaiComponent extends Component {
 	function is_emobile(){
 		return $this->_lib3gk->is_emobile();
 	}
-	
-	
+
+
 	/**
 	 * iPhone端末かのチェック
 	 * 詳しくはLib3gkCarrier::is_iphone()を参照
@@ -327,8 +327,8 @@ class KtaiComponent extends Component {
 	function is_iphone(){
 		return $this->_lib3gk->is_iphone();
 	}
-	
-	
+
+
 	/**
 	 * Android端末かのチェック
 	 * 詳しくはLib3gkCarrier::is_android()を参照
@@ -339,8 +339,8 @@ class KtaiComponent extends Component {
 	function is_android(){
 		return $this->_lib3gk->is_android();
 	}
-	
-	
+
+
 	/**
 	 * 携帯かチェック
 	 * 詳しくはLib3gkCarrier::is_ktai()を参照
@@ -351,8 +351,8 @@ class KtaiComponent extends Component {
 	function is_ktai(){
 		return $this->_lib3gk->is_ktai();
 	}
-	
-	
+
+
 	/**
 	 * PHSかチェック
 	 * 詳しくはLib3gkCarrier::is_phs()を参照
@@ -363,8 +363,8 @@ class KtaiComponent extends Component {
 	function is_phs(){
 		return $this->_lib3gk->is_phs();
 	}
-	
-	
+
+
 	/**
 	 * docomoのメールアドレスかチェック
 	 * 詳しくはLib3gkCarrier::is_imode_email()を参照
@@ -376,8 +376,8 @@ class KtaiComponent extends Component {
 	function is_imode_email($email){
 		return $this->_lib3gk->is_imode_email($email);
 	}
-	
-	
+
+
 	/**
 	 * SoftBankのメールアドレスかチェック
 	 * 詳しくはLib3gkCarrier::is_softbank_email()を参照
@@ -389,8 +389,8 @@ class KtaiComponent extends Component {
 	function is_softbank_email($email){
 		return $this->_lib3gk->is_softbank_email($email);
 	}
-	
-	
+
+
 	/**
 	 * vodafoneのメールアドレスかチェック
 	 * 詳しくはLib3gkCarrier::is_vodafone_email()を参照
@@ -402,8 +402,8 @@ class KtaiComponent extends Component {
 	function is_vodafone_email($email){
 		return $this->_lib3gk->is_vodafone_email($email);
 	}
-	
-	
+
+
 	/**
 	 * J-PHONEのメールアドレスかチェック
 	 * 詳しくはLib3gkCarrier::is_jphone_email()を参照
@@ -415,8 +415,8 @@ class KtaiComponent extends Component {
 	function is_jphone_email($email){
 		return $this->_lib3gk->is_jphone_email($email);
 	}
-	
-	
+
+
 	/**
 	 * AUのメールアドレスかチェック
 	 * 詳しくはLib3gkCarrier::is_ezweb_email()を参照
@@ -428,8 +428,8 @@ class KtaiComponent extends Component {
 	function is_ezweb_email($email){
 		return $this->_lib3gk->is_ezweb_email($email);
 	}
-	
-	
+
+
 	/**
 	 * EMOBILEのメールアドレスかチェック
 	 * 詳しくはLib3gkCarrier::is_emobile_email()を参照
@@ -441,8 +441,8 @@ class KtaiComponent extends Component {
 	function is_emobile_email($email){
 		return $this->_lib3gk->is_emobile_email($email);
 	}
-	
-	
+
+
 	/**
 	 * iPhoneのメールアドレスかチェック
 	 * 詳しくはLib3gkCarrier::is_iphone_email()を参照
@@ -454,8 +454,8 @@ class KtaiComponent extends Component {
 	function is_iphone_email($email){
 		return $this->_lib3gk->is_iphone_email($email);
 	}
-	
-	
+
+
 	/**
 	 * 携帯のメールアドレスかチェック
 	 * 詳しくはLib3gkCarrier::is_ktai_email()を参照
@@ -467,8 +467,8 @@ class KtaiComponent extends Component {
 	function is_ktai_email($email){
 		return $this->_lib3gk->is_ktai_email($email);
 	}
-	
-	
+
+
 	/**
 	 * PHSのメールアドレスかチェック
 	 * 詳しくはLib3gkCarrier::is_phs_email()を参照
@@ -480,8 +480,8 @@ class KtaiComponent extends Component {
 	function is_phs_email($email){
 		return $this->_lib3gk->is_phs_email($email);
 	}
-	
-	
+
+
 	/**
 	 * メールアドレスからキャリアコードの入手
 	 * 詳しくはLib3gkCarrier::get_email_carrier()を参照
@@ -493,8 +493,8 @@ class KtaiComponent extends Component {
 	function get_email_carrier($email){
 		return $this->_lib3gk->get_email_carrier($email);
 	}
-	
-	
+
+
 	/**
 	 * 絵文字の入手
 	 * 詳しくはLib3gkEmoji::emoji()を参照
@@ -511,8 +511,8 @@ class KtaiComponent extends Component {
 	function emoji($code, $disp = true, $carrier = null, $input_encoding = null, $output_encoding = null, $binary = null){
 		return $this->_lib3gk->emoji($code, $disp, $carrier, $output_encoding, $binary);
 	}
-	
-	
+
+
 	/**
 	 * 絵文字変換
 	 * 詳しくはLib3gkEmoji::convert_moji()を参照
@@ -529,8 +529,8 @@ class KtaiComponent extends Component {
 	function convert_emoji(&$str, $carrier = null, $input_encoding = null, $output_encoding = null, $binary = null){
 		return $this->_lib3gk->convert_emoji($str, $carrier, $input_encoding, $output_encoding, $binary);
 	}
-	
-	
+
+
 	/**
 	 * 機種情報の入手
 	 * 詳しくはLib3gkMachine::get_machineinfo()を参照
@@ -543,8 +543,8 @@ class KtaiComponent extends Component {
 	function get_machineinfo($carrier = null, $name = null){
 		return $this->_lib3gk->get_machineinfo($carrier, $name);
 	}
-	
-	
+
+
 	/**
 	 * 端末UIDの入手
 	 * 詳しくはLib3gkTools::get_uid()を参照
@@ -555,6 +555,6 @@ class KtaiComponent extends Component {
 	function get_uid(){
 		return $this->_lib3gk->get_uid();
 	}
-	
-	
+
+
 }
